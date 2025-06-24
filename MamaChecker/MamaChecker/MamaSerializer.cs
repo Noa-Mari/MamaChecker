62e3530d7a696ccb99f76858c105b1f11c943044
Noa-Mari <Noa.mari123@gmail.com>
2025-06-24 18:10:32 +0300

    add MamaSerializer.cs scrip to Serializer T list

---
+++ MamaChecker/MamaChecker/MamaSerializer.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace MamaChecker
+{
+    public class MamaSerializer
+    {
+        Stream Serialize<T>(T[] items)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T[]));
+
+            MemoryStream stream = new MemoryStream();
+            serializer.Serialize(stream, items);
+            stream.Position = 0;
+
+            return stream;
+        }
+
+    }
+}
